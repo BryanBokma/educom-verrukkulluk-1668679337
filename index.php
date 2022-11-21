b1e23b0ec2971d21349e3cef9eab1b9169175395
Bryan Bokma <bryanbokma4@gmail.com>
2022-11-21 17:04:09 +0100

    09 ingredient nog aanpassen (#9)

--- index.php
+++ index.php
@@ -2,14 +2,23 @@
 
 require_once("lib/database.php");
 require_once("lib/artikel.php");
+require_once("lib/user.php");
+require_once("kitchen_type");
+require_once("lib/ingredient.php");
 
 /// INIT
 $db = new database();
 $art = new artikel($db->getConnection());
+$user = new user($db->getConnection());
+$kitchen_type = new kitchen_type(db->getConnection());
+$ingredient = new ingredient($db->getConnection());
 
 
 /// VERWERK 
 $data = $art->selecteerArtikel(8);
+$data = $user->selecteerUser();
+$data = $kitchen_type->selecteerkitchen_type();
+$data = $ingredient->selecteeringredient();
 
 /// RETURN
 var_dump($data);
---
+++ lib/ingredient.php
@@ -0,0 +1,47 @@
+<?php
+
+class ingredient {
+    private $connection;
+    private $art;
+    
+    public function __construct($connection) {
+    $this->$connection = $connection;
+    $this->art = new artikel($connection);
+    }
+
+    private function selectArtikel($art_id) {
+        $data = $this->art->selecteerArtikel($art_id);
+        return($data);
+    }
+    
+    public function selecteeringredient($gerecht_id) {
+
+        $sql = "select * from ingredient where id = $gerecht_id";
+        $return = [];
+
+        $result = mysqli_query($this->$connection, $sql);
+
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+
+            $art_id = $row["artikel_id"];
+            $artikel = $this->selectArtikel(art_id);
+
+            $return[] = [
+
+                "id" => $row["id"],
+                "gerechtd_id" => $row["gerecht_id"],
+                "artikel_id" => $row["artikel_id"],
+                "aantal" => $row["aantal"],
+                "naam" => $artikel["naam"],
+                "omschrijving" => $artikel["omschrijving"],
+            ];
+            
+        }// end while function
+
+        return($return);
+
+    }// end public function selecteeringredient
+
+}// end class ingredient
+
+?>
