4a5e4aebee7ed358c2bc8431178659fe38c13a38
renekrewinkel <rene@studiokrijst.com>
2022-12-14 13:58:50 +0100

    update main.html.twig

--- lib/database.php
+++ lib/database.php
@@ -2,8 +2,8 @@
 
 // Aanpassen naar je eigen omgeving
 define("USER", "root");
-define("PASSWORD", "");
-define("DATABASE", "verrukkulluk");
+define("PASSWORD", "root");
+define("DATABASE", "verbb");
 define("HOST", "localhost");
 
 class database {
